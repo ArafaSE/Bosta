08cc092f3107b4a981420ffa9178904f382bd90b
ArafaSE <mohamed.arafa.swt@gmail.com>
2020-07-05 09:36:35 +0200

    Add Headless testing driver into our project to run tests without GUI

--- src/test/java/tests/business/TestBase.java
+++ src/test/java/tests/business/TestBase.java
@@ -4,7 +4,11 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
 import utilities.Helper;
@@ -29,14 +33,38 @@ public class TestBase extends AbstractTestNGCucumberTests {
 
     @BeforeSuite
     @Parameters({"browser"})
-    public void startDriver(@Optional("chrome") String browserName){
+    public void startDriver(@Optional("chrome-headless") String browserName){
         if (browserName.equalsIgnoreCase("chrome")){
-            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\drivers\\chromedriver.exe");
+            System.setProperty("webdriver.chrome.driver",
+                    System.getProperty("user.dir") + "\\drivers\\chromedriver.exe");
             System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
             driver = new ChromeDriver();
         } else if (browserName.equalsIgnoreCase("firefox")){
-            System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "\\drivers\\geckodriver.exe");
+            System.setProperty("webdriver.gecko.driver",
+                    System.getProperty("user.dir") + "\\drivers\\geckodriver.exe");
             driver = new FirefoxDriver();
+        }
+        // headless browser testing with phantomJs
+        else if (browserName.equalsIgnoreCase("headlessPhantomJs")) {
+            DesiredCapabilities caps = new DesiredCapabilities();
+            caps.setJavascriptEnabled(true);
+            caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
+                    System.getProperty("user.dir") + "\\drivers\\phantomjs.exe");
+            String[] phantomJsArgs = {"--web-security=no", "--ignore-ssl-errors=yes"};
+            caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, phantomJsArgs);
+
+            driver = new PhantomJSDriver(caps);
+        }
+        // headless browser testing with Chrome headless option
+        else if (browserName.equalsIgnoreCase("chrome-headless")) {
+            System.setProperty("webdriver.chrome.driver",
+                    System.getProperty("user.dir") + "\\drivers\\chromedriver.exe");
+            System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
+
+            ChromeOptions option = new ChromeOptions();
+            option.addArguments("--headless");
+            option.addArguments("--window-size=1920,1080");
+            driver = new ChromeDriver(option);
         }
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
